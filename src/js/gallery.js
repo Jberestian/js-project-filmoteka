c93b48bbbf0e064597b76c92bdc90548be7d23a4
Sergei Maznoi <s.maznoi888@gmail.com>
2022-06-13 20:28:43 +0300

    push

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,4 +1,5 @@
 import { TheMovieApi } from './themovie-api';
+import { onClickGallery } from "./modal-home";
 
 const containerEl = document.querySelector('.film__container');
 const listEl = document.querySelector('.film__list');
@@ -7,60 +8,51 @@ const theMovieApi = new TheMovieApi();
 const filmsPromiseEl = theMovieApi.fetchTrendsFilms();
 const filmsGenresEl = theMovieApi.fetchGenresFilms();
 
-// let movieId = [];
-
-// function setGenres() {
-//   let filmsGenres = theMovieApi
-//     .fetchGenresFilms()
-
-//     .then(id => {
-//       return (movieId = id.genres);
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     });
-// }
-// setGenres();
 
 filmsPromiseEl.then(result => {
   const films = result.data.results;
   trendsFilms(films);
 });
 
-// filmsGenresEl.then(response => {
-//   const genreResponse = response.data.genres;
-//   const genNameEl = genreResponse.map(genre => genre.name);
-//   const genIdEl = genreResponse.map(genre => genre.id);
+filmsGenresEl.then(response => {
+  const genreResponse = response.data.genres;
+  const genNameEl = genreResponse.map(genre => genre.name);
+  const genIdEl = genreResponse.map(genre => genre.id);
 
-//   console.log(genNameEl);
-//   console.log(genIdEl);
-//   localStorage.setItem(genIdEl, genNameEl);
-// });
 
-function recievedGenres(genIdEl) {
-    // console.log(genNameEl);
-    // console.log(genIdEl);
+  localStorage.setItem('id', genIdEl);
+  localStorage.setItem('name',genNameEl);
 
+  
+
+
+});
+
+const idOfFilms = localStorage.getItem('id').split(',');
+const nameOfGenres = localStorage.getItem('name').split(',');
+
+export function getNumberFilms(item) {
+  const numbersId = idOfFilms.indexOf(`${item}`);
+  return nameOfGenres[numbersId];
+ 
 }
 
-// let movieGenre = movieId
-//   .filter(genre => genre_ids.includes(genre.id))
-//   .map(genre => genre.name)
-//   .join(', ');
 
 function trendsFilms(films) {
-    
-
   const markupItems = films
     .map(film => {
+      
       return `
-        <li class="film__item">
+        <li class="film__item" id="${film.id}">
           <img class="film__img" src="https://image.tmdb.org/t/p/w500/${
             film.poster_path
-          }" alt=${film.original_title} id="${film.id}">
+          }" alt=${film.original_title}>
           <h3 class="film__name">${film.title}</h3>
           <p class="film__genre">
-            ${film.genre_ids}
+            ${film.genre_ids.map(item => {
+              item = getNumberFilms(item)
+              return` ${item}`
+             })}
             <span class="film__date-release">| ${film.release_date.slice(
               0,
               4
@@ -70,4 +62,6 @@ function trendsFilms(films) {
     })
     .join('');
   listEl.insertAdjacentHTML('beforeend', markupItems);
+  const filmsCheck = document.querySelectorAll('.film__item')
+  filmsCheck.forEach(film => film.addEventListener('click', onClickGallery))
 }
